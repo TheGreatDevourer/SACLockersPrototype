b4c78eae3e2f8d42c24b77e666b9fe2c1c1fea34
TheGreatDevourer <anthonygould2304@outlook.com>
2023-01-02 22:43:41 -0400

    Unit Test complete and Rework Rent and RentType

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,10 @@
 import os, tempfile, pytest, logging, unittest
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from App.main import create_app
-from App.database import create_db
-from App.models import User
-from App.controllers import (
+from main import create_app
+from database import create_db
+from models import User
+from controllers import (
     create_user,
     get_all_users_json,
     authenticate,
