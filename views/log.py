03d07c3879ecddb6307964c66d994549695dd68e
adonispaul722 <adonispaul7232@gmail.com>
2023-01-13 02:15:26 +0000

    log page

--- views/log.py
+++ views/log.py
@@ -5,6 +5,8 @@ from controllers import (
     get_all_logs
     )
 
+log = Blueprint('log', __name__, template_folder='../templates')
+
 @log.route('/log', methods=['GET'])
 def Log_page():
     return render_template('log.html', log = get_all_logs())
